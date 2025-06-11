16b8eafcc25508974c7792ee48e754c864948b2b
AreebaAtif <bscs23179@itu.edu.pk>
2025-06-12 02:50:40 +0500

    created the profile page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Feed from "./components/Feed";
-import ProjectDetail from "./components/ProjectDetail";
-import CreateProject from "./components/CreateProject";
-import SavedProjects from "./components/SavedProjects";
-import Profile from "./components/Profile";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/project/:id" element={<ProjectDetail />} />
-          <Route path="/create" element={<CreateProject />} />
-          <Route path="/saved" element={<SavedProjects />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
